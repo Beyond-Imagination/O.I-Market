2c4df2d78f1b649a15f0897e6133ed0335272879
laggu <laggu91@gmail.com>
2019-12-06 14:37:30 +0900

    update the layout of the album component

--- web/src/components/Album.js
+++ web/src/components/Album.js
@@ -1,18 +1,7 @@
 import React, {Component} from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
 
 const styles = theme => ({
   icon: {
@@ -50,52 +39,88 @@ const styles = theme => ({
   },
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-class Album extends Component {
-
-    render () {
-        const {classes} = this.props;
-        return (
-            <React.Fragment>
-              <main className={classes.content}>
-                <Container className={classes.cardGrid} >
-                  {/* End hero unit */}
-                  <Grid container spacing={4}>
-                    {cards.map(card => (
-                      <Grid item key={card} xs={12} sm={6} md={3} lg={2} xl={1} >
-                        <Card className={classes.card}>
-                          <CardMedia
-                            className={classes.cardMedia}
-                            image="https://source.unsplash.com/random"
-                            title="Image title"
-                          />
-                          <CardContent className={classes.cardContent}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                              Heading
-                            </Typography>
-                            <Typography>
-                              This is a media card. You can use this section to describe the content.
-                            </Typography>
-                          </CardContent>
-                          <CardActions>
-                            <Button size="small" color="primary">
-                              View
-                            </Button>
-                            <Button size="small" color="primary">
-                              Edit
-                            </Button>
-                          </CardActions>
-                        </Card>
-                      </Grid>
-                    ))}
-                  </Grid>
-                </Container>
-              </main>
-            </React.Fragment>
-          );
-    }
+const tileData = [
+   {
+      img: "https://source.unsplash.com/random",
+     title: 'Image',
+     author: 'author',
+     cols: 1,
+   },
+   {
+    img: "https://source.unsplash.com/random",
+   title: 'Image',
+   author: 'author',
+   cols: 2,
+ },{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 3,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 2,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 4,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 3,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 2,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 5,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 5,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 3,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 4,
+},{
+  img: "https://source.unsplash.com/random",
+ title: 'Image',
+ author: 'author',
+ cols: 1,
+},
+ ];
   
+class Album extends Component {
+  render () {
+    const {classes} = this.props;
+    return (
+      <React.Fragment>
+        <main className={classes.content}>
+          <GridList cellHeight={160} className={classes.cardGrid} cols={12}>
+            {tileData.map(tile => (
+              <GridListTile key={tile.img} cols={tile.cols || 1}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(Album);
