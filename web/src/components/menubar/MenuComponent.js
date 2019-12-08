7a8d4d36d0be9acae9d5603ae436b86b7ba24ed9
xoelxowl <xpelxowl@naver.com>
2019-12-08 20:53:56 +0900

    구조에 대해서 파악했습니다 차주 부터는 파일 내부를 확인할께요

--- web/src/components/menubar/MenuComponent.js
+++ web/src/components/menubar/MenuComponent.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 import MenuBarComponent from './MenuBarComponent';
 import DrawerComponent from './DrawerComponent';
 
+
 class MenuComponent extends Component {
     constructor(props) {
         super(props);
